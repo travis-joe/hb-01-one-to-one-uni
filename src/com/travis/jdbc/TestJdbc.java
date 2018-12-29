e864f6542e9667b4ce2a3e7ba2bbdc52226faa04
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-12-29 10:44:33 +0800

    210 entity

---
+++ src/com/travis/jdbc/TestJdbc.java
@@ -0,0 +1,17 @@
+package com.travis.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTImezone=UTC";
+        String user = "root";
+        String password = "Weeqwe123";
+        try {
+            Connection con = DriverManager.getConnection(jdbcUrl, user, password);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
